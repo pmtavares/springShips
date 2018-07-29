708e3b3f51051793a34db727ff2dba5911483ea6
pmtavares <pedrotavares41@gmail.com>
2018-07-29 20:17:21 +0100

    First Commit

---
+++ src/test/java/com/boot/ShipwreckRepositoryIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.boot;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class ShipwreckRepositoryIntegrationTest {
+	
+	@Autowired
+	private ShipwreckRepository shipwreckRepository;
+	
+	@Test
+	public void testFindAll()
+	{
+		List<Shipwreck> wrecks = shipwreckRepository.findAll();
+		assertThat(wrecks.size(), is(greaterThanOrEqualTo(0)));
+		
+	}
+
+}
